bb858709700b36895b40ad86737b463b34208e7b
AmirrShadid <amirrshadid@gmail.com>
2026-07-22 14:39:11 +0800

    Typo whatsaps

--- app/page.tsx
+++ app/page.tsx
@@ -488,7 +488,7 @@ export default function BookingPage() {
               />
             </div>
             <div>
-              <label className="block text-[10px] font-bold tracking-widest text-stone-400 uppercase mb-1">Phone Number (Wwhatsapp)</label>
+              <label className="block text-[10px] font-bold tracking-widest text-stone-400 uppercase mb-1">Phone Number (Whatsapp)</label>
               <input
                 type="tel"
                 required
